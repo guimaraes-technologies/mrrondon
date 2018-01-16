403f28ec521d0297c07b6cf8fce9a6c9bf0892f6
Oziel Guimaraes <contato@ozielguimaraes.net>
2018-01-16 17:49:00 -0400

    Added accountcontroller

---
+++ src/MrRondon.Presentation.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace MrRondon.Presentation.Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Signin(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View(new LoginVm { ReturnUrl = returnUrl });
+        }
+
+        [AllowAnonymous, HttpPost]
+        public ActionResult Signin(LoginVm model)
+        {
+            return View(model);
+        }
+    }
+}
