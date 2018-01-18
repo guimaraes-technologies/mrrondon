86f7f903b91e2260b418fc10697a55859978ae5c
Oziel Guimaraes <contato@ozielguimaraes.net>
2018-01-18 12:24:48 -0400

    Added city controller

---
+++ src/MrRondon.Services.Api/Controllers/CityController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using MrRondon.Services.Api.Context;
+
+namespace MrRondon.Services.Api.Controllers
+{
+    [RoutePrefix("v1/city")]
+    public class CityController : ApiController
+    {
+        private readonly MainContext _db;
+
+        public CityController()
+        {
+            _db = new MainContext();
+        }
+
+        [AllowAnonymous]
+        [Route("{name=}")]
+        public IHttpActionResult Get(string name)
+        {
+            try
+            {
+                name = name ?? string.Empty;
+                return Ok(_db.Cities.Where(x => x.Name.Contains(name)));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
