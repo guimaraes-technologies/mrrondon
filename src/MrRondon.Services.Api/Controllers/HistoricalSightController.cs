1ecf85b7d1f1fc6319c015b14a9f1c3217bb0a2f
Oziel Guimaraes <contato@ozielguimaraes.net>
2018-01-18 12:17:59 -0400

    Added historicalsight controller

---
+++ src/MrRondon.Services.Api/Controllers/HistoricalSightController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using MrRondon.Services.Api.Context;
+
+namespace MrRondon.Services.Api.Controllers
+{
+    [RoutePrefix("v1/historicalsight")]
+    public class HistoricalSightController : ApiController
+    {
+        private readonly MainContext _db;
+
+        public HistoricalSightController()
+        {
+            _db = new MainContext();
+        }
+
+        [AllowAnonymous]
+        [Route("{name=}")]
+        public IHttpActionResult Get(string name)
+        {
+            try
+            {
+                name = name ?? string.Empty;
+                return Ok(_db.HistoricalSights.Where(x => x.Name.Contains(name)));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
