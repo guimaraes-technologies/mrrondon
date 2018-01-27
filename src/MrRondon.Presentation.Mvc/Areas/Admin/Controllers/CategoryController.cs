a1e9a4bf8f8a468f3db6a54fd74bb7b6269919aa
Oziel Guimaraes <contato@ozielguimaraes.net>
2018-01-27 13:19:17 -0400

    Added category controller

---
+++ src/MrRondon.Presentation.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MrRondon.Domain.Entities;
+using MrRondon.Infra.Data.Context;
+
+namespace MrRondon.Presentation.Mvc.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    public class CategoryController : Controller
+    {
+        private readonly MainContext _db = new MainContext();
+
+        public ActionResult Index()
+        {
+            return View(_db.Categories.Where(x => x.SubCategoryId == null).ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category == null) return HttpNotFound();
+            return View(category);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryId,Name,Image,SubCategoryId")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category == null) return HttpNotFound();
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryId,Name,Image,SubCategoryId")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(category).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = _db.Categories.Find(id);
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
