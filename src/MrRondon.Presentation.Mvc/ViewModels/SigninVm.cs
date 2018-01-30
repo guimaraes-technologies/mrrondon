78784f3dae8d6d03b8b9455f57d7c7f182b4e351
Oziel Guimaraes <contato@ozielguimaraes.net>
2018-01-30 14:13:07 -0400

    Adding crosscutting

---
+++ src/MrRondon.Presentation.Mvc/ViewModels/SigninVm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using MrRondon.Infra.CrossCutting.Message;
+
+namespace MrRondon.Presentation.Mvc.ViewModels
+{
+    public class SigninVm
+    {
+        [DisplayName("Login")]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Error))]
+        public string UserName { get; set; }
+
+        [DisplayName("Senha")]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Error))]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
