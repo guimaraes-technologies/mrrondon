1e32c87a7b696219eda8fa57346c13673fc1cfea
Oziel Guimaraes <microzapple@gmail.com>
2018-02-02 16:45:51 -0400

    Fix singin layout

---
+++ src/MrRondon.Presentation.Mvc/Scripts/site/carrossel.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    "use strict";
+
+    window.Carrossel = window.Carrossel || {};
+
+    Carrossel.Element = $(".gt-carrossel");
+
+    Carrossel.Values = Carrossel.Element.data();
+
+    setInterval(function () {
+        var random = Math.floor((Math.random() * Carrossel.Values.carrosselQtd) + 1);
+        var imgFadeIn = $("<div>").css("background-image", `url(${Carrossel.Values.carrosselFile}${random}.png)`);
+        var imgFadeOut = Carrossel.Element.find("div");
+        Carrossel.Element.prepend(imgFadeIn);
+        imgFadeOut.fadeOut(2500, function () { imgFadeOut.remove() });
+    }, 5000);
+});
