06779181395bf21661b0f4b48a13aeeb1e648c7e
Oziel Guimaraes <contato@ozielguimaraes.net>
2018-02-03 15:43:38 -0400

    Add search by id for company

--- src/MrRondon.Services.Api/Controllers/CompanyController.cs
+++ src/MrRondon.Services.Api/Controllers/CompanyController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Data.Entity;
 using System.Web.Http;
 using MrRondon.Infra.Data.Context;
 
@@ -13,6 +14,25 @@ namespace MrRondon.Services.Api.Controllers
         public CompanyController()
         {
             _db = new MainContext();
+        }
+
+
+        [AllowAnonymous]
+        [Route("{id:int}")]
+        public IHttpActionResult Get(Guid id)
+        {
+            try
+            {
+                var item = _db.Companies
+                    .Include(i => i.Address.City)
+                    .Include(s => s.Segment.SubCategory)
+                    .FirstOrDefault(f => f.CompanyId == id);
+                return Ok(item);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [AllowAnonymous]
