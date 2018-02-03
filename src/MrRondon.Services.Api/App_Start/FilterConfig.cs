722eaf3ae5661e1fa518f28fa2835028c1b45dd0
Oziel Guimaraes <contato@ozielguimaraes.net>
2018-02-03 11:11:05 -0400

    Add method to check and uncheck favorite event

--- src/MrRondon.Services.Api/App_Start/FilterConfig.cs
+++ src/MrRondon.Services.Api/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace MrRondon.Services.Api
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
--- src/MrRondon.Services.Api/Controllers/UserController.cs
+++ src/MrRondon.Services.Api/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
+using MrRondon.Domain.Entities;
 using MrRondon.Infra.Data.Context;
 using MrRondon.Services.Api.Authorization;
 
@@ -36,12 +37,52 @@ namespace MrRondon.Services.Api.Controllers
         {
             try
             {
-                return Ok(_db.FavoriteEvents.Include(i => i.Event).Where(x => x.UserId == Authentication.Current.UserId).Select(s => s.Event));
+                return Ok(_db.FavoriteEvents.Include(i => i.Event).Where(x => x.UserId == Authentication.Current.UserId));
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpPost]
+        [Route("event/favorite")]
+        public IHttpActionResult MarkAsFavorite([FromBody]FavoriteEvent model)
+        {
+            try
+            {
+                _db.FavoriteEvents.Add(model);
+                _db.SaveChangesAsync();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("event/unfavorite")]
+        public IHttpActionResult UnMarkAsFavorite([FromBody]FavoriteEvent model)
+        {
+            try
+            {
+                _db.FavoriteEvents.Remove(model);
+                _db.SaveChangesAsync();
+
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) _db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
