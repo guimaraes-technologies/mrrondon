f78ab16a38cae3da905d39b74695e6a88d8fe489
Oziel Guimaraes <microzapple@gmail.com>
2018-02-07 11:35:57 -0400

    Layout of category image

--- src/MrRondon.Infra.CrossCutting.Helper/Buttons/ButtonsCategory.cs
+++ src/MrRondon.Infra.CrossCutting.Helper/Buttons/ButtonsCategory.cs
@@ -16,7 +16,7 @@ namespace MrRondon.Infra.CrossCutting.Helper.Buttons
             {
                 var link = new TagBuilder("img");
                 link.MergeAttribute("class", "ui tiny icon");
-                link.MergeAttribute("style", "height: 60px; width: 60px;");
+                link.MergeAttribute("style", "height: 70px; width: 70px;");
                 link.MergeAttribute("src", array != null && array.Length > 0 ? $"data:image/PNG;base64,{Convert.ToBase64String(array)}" : "/Content/Images/without_image.jpg");
                 link.MergeAttribute("title", "Imagem");
                 link.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(null));
@@ -34,7 +34,7 @@ namespace MrRondon.Infra.CrossCutting.Helper.Buttons
                 var iconEdit = new TagBuilder("i");
                 iconEdit.MergeAttribute("class", IconDetails);
                 link.MergeAttribute("href", Url.Action("Details", "Category", new { area = "Admin", id }));
-                link.MergeAttribute("class", "ui small image");
+                link.MergeAttribute("class", "ui tiny icon button");
                 link.MergeAttribute("data-loading", "btn");
                 link.MergeAttribute("title", "Detalhes");
                 link.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(null));
