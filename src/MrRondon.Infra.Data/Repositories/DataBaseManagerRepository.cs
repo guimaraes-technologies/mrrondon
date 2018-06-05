e6427ff47ec8401e002d418e3b7a6443f63eee5f
Oziel Guimarães <microzapple@gmail.com>
2018-06-05 10:18:12 -0400

    Adding implementation of databasemanager repository

---
+++ src/MrRondon.Infra.Data/Repositories/DataBaseManagerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using MrRondon.Domain.Interfaces.Repositories;
+using MrRondon.Infra.Data.Context;
+using MrRondon.Infra.Data.Migrations;
+
+namespace MrRondon.Infra.Data.Repositories
+{
+    public class DataBaseManagerRepository : IDataBaseManagerRepository
+    {
+        public void UpdateToLastedVersion()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MainContext, Configuration>());
+        }
+    }
+}
