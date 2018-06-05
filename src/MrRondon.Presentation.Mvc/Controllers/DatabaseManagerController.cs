1a6decc146748d242395d16fbbec6b0c96488985
Oziel Guimarães <microzapple@gmail.com>
2018-06-05 10:30:44 -0400

    Adding controller with the method to update the database for the lasted version

---
+++ src/MrRondon.Presentation.Mvc/Controllers/DatabaseManagerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using MrRondon.Infra.Data.Repositories;
+using MrRondon.Presentation.Mvc.Extensions;
+
+namespace MrRondon.Presentation.Mvc.Controllers
+{
+    public class DatabaseManagerController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Update(string code)
+        {
+            try
+            {
+                var databaseManager = new DataBaseManagerRepository();
+                databaseManager.UpdateToLastedVersion();
+                return RedirectToAction("Index", "Home").Success("Banco de dados atualizado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home").Error(ex.Message);
+            }
+        }
+    }
+}
